3b13f5e624efbb52d8f88a17b6e8d1d755aca06a
ykajita722 <ykajita@mathworks.com>
2020-06-15 21:01:55 +0900

    add test cases

--- test_unittest/SolverTest.m
+++ test_unittest/SolverTest.m
@@ -8,7 +8,18 @@ classdef SolverTest < matlab.unittest.TestCase
             expSolution = [2,1];
             testCase.verifyEqual(actSolution,expSolution);
         end
+        function testRealSolution_fail(testCase)
+            actSolution = quadraticSolver(1,-3,2);
+            expSolution = [2,1];
+            testCase.verifyEqual(actSolution,expSolution+1);
+        end
         function testImaginarySolution(testCase)
+            actSolution = quadraticSolver(1,2,10);
+            expSolution = [-1+3i, -1-3i];
+            testCase.verifyEqual(actSolution,expSolution);
+        end
+        
+        function testImaginarySolution_fail(testCase)
             actSolution = quadraticSolver(1,2,10);
             expSolution = [-1+3i, -1-3i];
             testCase.verifyEqual(actSolution,expSolution);
